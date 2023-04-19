6f8861a77340e1e75140099291ea3453db0b651f
MateuszJaz <mateusz.jaz@outlook.com>
2023-04-19 18:17:46 +0200

    validator update

--- utils/validators/validator.js
+++ utils/validators/validator.js
@@ -13,8 +13,10 @@ const contactValidation = Joi.object({
       'string.pattern.base': `Phone number must be written as 777-777-777.`,
     })
     .required(),
+  favorite: Joi.boolean().optional(),
 });
-const updateContact = Joi.object({
+
+const contactValidationUpdate = Joi.object({
   name: Joi.string().min(3).max(30).optional(),
   email: Joi.string()
     .email({
@@ -28,9 +30,14 @@ const updateContact = Joi.object({
       'string.pattern.base': `Phone number must be written as 777-777-777.`,
     })
     .optional(),
+  favorite: Joi.boolean().optional(),
 }).min(1);
 
-const validate = (schema, obj, res, next) => {
+const contactValidationStatus = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const validate = (schema, obj, next, res) => {
   const { error } = schema.validate(obj);
   if (error) {
     const [{ message }] = error.details;
@@ -44,10 +51,14 @@ const validate = (schema, obj, res, next) => {
   next();
 };
 
-module.exports.contactValid = (req, res, next) => {
-  return validate(contactValidation, req.body, res, next);
+module.exports.createContact = (req, res, next) => {
+  return validate(contactValidation, req.body, next, res);
 };
 
-module.exports.contactUpdate = (req, res, next) => {
-  return validate(updateContact, req.body, res, next);
+module.exports.updateContact = (req, res, next) => {
+  return validate(contactValidationUpdate, req.body, next, res);
+};
+
+module.exports.updateStatus = (req, res, next) => {
+  return validate(contactValidationStatus, req.body, next, res);
 };
