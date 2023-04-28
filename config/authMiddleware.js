07c9adf893118375639e30233b261fe73b8ce011
MateuszJaz <mateusz.jaz@outlook.com>
2023-04-28 17:28:58 +0200

    authMiddleware fix - user token validation added

--- config/authMiddleware.js
+++ config/authMiddleware.js
@@ -2,7 +2,10 @@ const passport = require('passport');
 
 const auth = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (!user || err) {
+    const authHeader = req.headers.authorization;
+    const reqToken = authHeader.split(' ')[1];
+
+    if (!user || user.token !== reqToken || err) {
       return res.status(401).json({
         status: 'error',
         code: 401,
